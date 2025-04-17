328b95d5d67fa6e015ba5ae2e51356e3dc8d00f4
Ismayil Karimli <ismayilkarimli@Ismayils-MacBook-Pro.local>
2025-04-17 18:51:13 +0300

    update 15

--- leetcode-codes/15_3sum.py
+++ leetcode-codes/15_3sum.py
@@ -2,28 +2,28 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         res = []
+        nums.sort()
 
-        for l in range(len(nums) - 2):
-            if l > 0 and nums[l] == nums[l - 1]:
+        for x in range(len(nums) - 2):
+            if x > 0 and nums[x] == nums[x - 1]:
                 continue
             
-            i, r = l + 1, len(nums) - 1
-            while i < r:
-                s = nums[l] + nums[i] + nums[r]
-                if s > 0:
-                    r -= 1
-                elif s < 0:
-                    i += 1
-                else:
-                    res.append([nums[l], nums[i], nums[r]])
-                    i += 1
-                    while nums[i] == nums[i - 1] and i < r:
-                        i += 1
-        
-        return res
+            l, r = x + 1, len(nums) - 1
 
+            while l < r:
+                sum = nums[x] + nums[l] + nums[r]
+                if sum < 0:
+                    l += 1
+                elif sum > 0:
+                    r -= 1
+                else:
+                    res.append([nums[x], nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+            
+        return res
 # Test
 solution = Solution()
 print(solution.threeSum([-1,0,1,2,-1,-4])) # [[-1,-1,2],[-1,0,1]]
