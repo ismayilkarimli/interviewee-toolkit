72cfa4cb3f4a98848d6f469d64cb4d4b2896d1b9
Ismayil Karimli <ismayilkarimli@Ismayils-MacBook-Pro.local>
2025-04-18 17:41:54 +0300

    update 42

--- leetcode-codes/42_trapping_rain_water.py
+++ leetcode-codes/42_trapping_rain_water.py
@@ -3,20 +3,21 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
-        left_max, right_max = height[l], height[r]
         total_water = 0
+        left_max, right_max = height[l], height[r]
 
         while l < r:
             if height[l] < height[r]:
-                l += 1
                 left_max = max(left_max, height[l])
                 total_water += left_max - height[l]
+                l += 1
             else:
-                r -= 1
                 right_max = max(right_max, height[r])
                 total_water += right_max - height[r]
+                r -= 1
         
         return total_water
+
 
 solution = Solution()
 print(solution.trap([0,1,0,2,1,0,1,3,2,1,2,1])) # 6
